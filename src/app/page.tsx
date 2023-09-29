87f6b028e99038b485a7b8e516e47bc9a55fd41f
Aniruddha Bagal <bagalaniruddha@gmail.com>
2023-09-30 02:01:50 +0530

    Added input component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,21 +10,10 @@ const Home: React.FC = () => {
   const [todo, setTodo] = useState<string>("");
   const [todos, setTodos] = useState<Todo[]>([]);
 
-  const handleAdd = (e: React.FormEvent) => {
-    e.preventDefault();
-
-    if (todo) {
-      setTodos([...todos, { id: Date.now(), todo, isDone: false }]);
-      setTodo("");
-    }
-  };
-
   console.log(todos);
   return (
     <div className={page.App}>
       <span className={page.heading}>Taskify</span>
-      <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
-      <TodoList todos={todos} setTodos={setTodos} />
     </div>
   );
 };
