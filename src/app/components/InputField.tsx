523c0b9087fe4252e270a0640fcafd62af33f787
Aniruddha Bagal <bagalaniruddha@gmail.com>
2023-09-30 02:03:10 +0530

    Added input component and interfaced it

---
+++ src/app/components/InputField.tsx
@@ -0,0 +1,36 @@
+import React, { useRef } from "react";
+import styles from "./styles.module.css";
+
+interface props {
+  todo: string;
+  setTodo: React.Dispatch<React.SetStateAction<string>>;
+  handleAdd: (e: React.FormEvent) => void;
+}
+
+const InputField: React.FC<props> = ({ todo, setTodo, handleAdd }) => {
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  return (
+    <form
+      className={styles.input}
+      onSubmit={(e) => {
+        handleAdd(e);
+        inputRef.current?.blur();
+      }}
+    >
+      <input
+        type="text"
+        placeholder="Enter a Task"
+        value={todo}
+        ref={inputRef}
+        onChange={(e) => setTodo(e.target.value)}
+        className={styles.input__box}
+      />
+      <button type="submit" className={styles.input_submit}>
+        GO
+      </button>
+    </form>
+  );
+};
+
+export default InputField;
